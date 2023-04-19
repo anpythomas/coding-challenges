fe6981822b1aaa122a7da615f0ea43cc563cca2e
Andy <andythomas.py@gmail.com>
2023-04-19 15:24:17 -0500

    Added updated solution

--- 4-18-2023/regexValidPinCode.js
+++ 4-18-2023/regexValidPinCode.js
@@ -1,11 +1,21 @@
 function validatePIN (pin) {
-    let newPin = pin.trim()
-    console.log(newPin.length, pin.length)
-
-    if (newPin.length === 4 || newPin.length === 6) {
-        return true
+    let pinTrimmed = pin.replaceAll(" ", "").trim()
+    
+    if ((pin.length === 4 || pin.length === 6) && (pin === pinTrimmed)) {
+      for (i = 0; i < pin.length; i++) {
+          if (pin[i] >= 0 && pin[i] <= 9) {
+            
+          } else {
+            return false
+          }
+      }
+    } else {
+      return false
     }
+  
+    return true
+      
 
 }
 
-console.log(validatePIN("123 "))
+console.log(validatePIN("55 675"))
