47323cfbdecb3aa8d978809607aef68a241418ba
Andy <andythomas.py@gmail.com>
2023-04-24 19:07:29 -0500

    Added 4-24 updated solution 1

--- 4-24-2023/grasshopperGradBook/js/main.js
+++ 4-24-2023/grasshopperGradBook/js/main.js
@@ -1,17 +1,28 @@
-function getGrade (s1, s2, s3) {
-    let avg = (s1 + s2 + s3) / 3
+// function getGrade (s1, s2, s3) {
+//     let avg = (s1 + s2 + s3) / 3
     
-    if (avg >= 90 && avg <=100) {
-      return "A"
-    } else if (avg >= 80 && avg < 90) {
-      return "B"
-    } else if (avg >= 70 && avg < 80) {
-      return "C"
-    } else if (avg >= 60 && avg < 70) {
-      return "D"
-    } else {
-      return "F"
-    }
-  }
+//     if (avg >= 90 && avg <=100) {
+//       return "A"
+//     } else if (avg >= 80 && avg < 90) {
+//       return "B"
+//     } else if (avg >= 70 && avg < 80) {
+//       return "C"
+//     } else if (avg >= 60 && avg < 70) {
+//       return "D"
+//     } else {
+//       return "F"
+//     }
+//   }
 
-console.log(getGrade(90, 83, 93))
+function getGrade(g1, g2, g3) {
+    let avg = (g1 + g2 + g3) / 3
+    console.log(avg)
+    if (avg >= 90) return "A";
+        else if (avg >= 80) return "B";
+        else if (avg >= 70) return "C";
+        else if (avg >=60) return "D";
+        else return "F";
+
+}
+
+console.log(getGrade(59, 60, 60))
