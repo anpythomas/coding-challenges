d05a22e7dd0aebe901f169ee4fa8bd66efef3f46
Andy <andythomas.py@gmail.com>
2023-04-29 11:02:17 -0500

    Added firstNonConsecutive 4-29 challenge 1.0

---
+++ 4-29-2023/js/main.js
@@ -0,0 +1,11 @@
+function firstNonConsecutive (arr) {
+    for (i = 0; i < arr.length; i++) {
+        console.log(arr[i] + 1, arr[i+1])
+      if (arr[i] + 1 !== arr[i+1]) {
+        return arr[i+1]
+      }
+    }
+    return null
+  }
+
+console.log(firstNonConsecutive([1, 2, 3, 4, 5, 6, 8]))
