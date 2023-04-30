c0aea4cf625bafb49d8dee2350bbfbf5b89458ae
Andy <andythomas.py@gmail.com>
2023-04-29 21:37:02 -0500

    Added solution points v1.0

--- 4-29-2023/totalAmountOfPoints/js/main.js
+++ 4-29-2023/totalAmountOfPoints/js/main.js
@@ -1,6 +1,16 @@
 function points(games) {
+
+    let myTeamSumPoints = 0;
+
     for (i = 0; i < games.length; i++) {
-        console.log(i)
+        if (Number(games[i].split(":")[0]) > Number(games[i].split(":")[1])) {
+            myTeamSumPoints += 3
+        } else if (Number(games[i].split(":")[0]) === Number(games[i].split(":")[1])) {
+            myTeamSumPoints += 1
+        }
+
     }
-    return 0
+    return myTeamSumPoints
   }
+
+console.log(points(["3:1", "2:2", "0:1"]))
