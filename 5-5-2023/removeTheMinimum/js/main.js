be5a8143d56140193d6793a14eb9808e2d670e1a
Andy <andythomas.py@gmail.com>
2023-05-05 21:49:51 -0500

    Added solution v.1.0 challenge 1

--- 5-5-2023/removeTheMinimum/js/main.js
+++ 5-5-2023/removeTheMinimum/js/main.js
@@ -1,4 +1,21 @@
 function removeSmallest(numbers) {
-    console.log(numbers)
-    return [];
+    let tmpLowestNum = numbers[0]
+    let lowestIndex = 0
+    let finalArray = []
+    for (i = 0; i < numbers.length; i++) {
+        if ( numbers[i] < tmpLowestNum ) {
+            tmpLowestNum = numbers[i]
+            lowestIndex = i
+        }
+    }
+    
+    for (i = 0; i < numbers.length; i++) {
+        if (i===lowestIndex) {
+        } else {
+            finalArray.push(numbers[i])
+        }
+    }
+    return finalArray;
   }
+
+console.log(removeSmallest([2, 2, 3, 4, 5]))
