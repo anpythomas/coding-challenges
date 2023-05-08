9e20f9538cb33ff1d61821ebd566fd944bd97b80
Andy <andythomas.py@gmail.com>
2023-05-08 10:15:57 -0500

    Added 5-8 challenge 2 v.1.1 solution

--- 5-8-2023/noOdditiesHere/main.js
+++ 5-8-2023/noOdditiesHere/main.js
@@ -1,8 +1,9 @@
-function noOdds( values ){
-    for (i = 0; i < values.length; i++) {
-      if (values[i] % 2 !== 0) {
-        values.splice(i, 1)
-      }
+function noOdds(values) {
+    let newArray = []
+    for (i = 0; i < values.length; i ++) {
+        if (values[i] % 2 === 0) {
+            newArray.push(values[i])
+        }
     }
-    return values
-  }
+    return newArray
+}
