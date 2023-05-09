a4e97f86da82e1d327cdd6546c121a8a10961658
Andy <andythomas.py@gmail.com>
2023-05-09 08:18:16 -0500

    Added solution countCharsInStr with ternary operator

--- 5-9-2023/countCharsInStr/js/main.js
+++ 5-9-2023/countCharsInStr/js/main.js
@@ -2,12 +2,10 @@ function count(string) {
     let myObj = {}
     
     for (i = 0; i < string.length; i++) {
-      if (string[i] in myObj) {
-        myObj[string[i]] += 1
-      } else {
-        myObj[string[i]] = 1
-      }
+        string[i] in myObj ? myObj[string[i]] += 1 : myObj[string[i]] = 1
     }
     
     return myObj;
   }
+
+console.log(count(['a', 'b', 'a']))
