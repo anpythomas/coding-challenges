7d0c3629b57a5de3a3e53b1b72972f51733eb423
Andy <andythomas.py@gmail.com>
2023-05-13 13:20:41 -0500

    Added 5-13 v.1.2 with ternary operators

--- 5-13-2023/countingDuplicates/js/main.js
+++ 5-13-2023/countingDuplicates/js/main.js
@@ -4,20 +4,11 @@ function duplicateCount(text){
     let numOfDups = 0
     
     for (i = 0; i < textLowerCase.length; i++) {
-        // If letter in object
-        if (textLowerCase[i] in charCounter){
-          // Add 1 to object key value pair for letter
-          charCounter[textLowerCase[i]] += 1
-        } else {
-          // Else, add key and 1 to object
-          charCounter[textLowerCase[i]] = 1
-        }
+        textLowerCase[i] in charCounter ? charCounter[textLowerCase[i]] += 1 : charCounter[textLowerCase[i]] = 1
     }
     
     for (const product in charCounter) {
-      if (charCounter[product] > 1) {
-        numOfDups += 1
-      }
+      charCounter[product] > 1 ? numOfDups += 1 : null
     }
     
     return numOfDups
