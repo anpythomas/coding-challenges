29d07f039cf1f81362b6da52a7fbe883612f1ef6
Andy <andythomas.py@gmail.com>
2023-05-12 19:16:27 -0500

    Added final solution with both ternary ops

--- 5-12-2023/theOfficeOneOutdated/js/main.js
+++ 5-12-2023/theOfficeOneOutdated/js/main.js
@@ -3,19 +3,13 @@ function outed(meet, boss){
     let totalPeople = 0
     let avg = 0
 
-    
     for (const property in meet) {
       totalPeople += 1
-      if (property === boss) {
-        sum += (meet[property] * 2)
-      } else {
-        sum += meet[property]
-      }
+      property === boss ? sum += (meet[property] * 2) : sum += meet[property]
     }
     
     avg = sum / totalPeople
     
     return avg <= 5 ? "Get Out Now!" : "Nice Work Champ!"
 
-  
   }
