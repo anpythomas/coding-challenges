dd28eaa077e305d28ab16d322b94e0f0fb86daaf
Andy <andythomas.py@gmail.com>
2023-05-16 15:41:12 -0500

    Added final solution v.1.4 ternary operator and map

--- 5-16-2023/categorizeNewMember/js/main.js
+++ 5-16-2023/categorizeNewMember/js/main.js
@@ -1,7 +1,3 @@
 function openOrSenior(data){
-    let category = []
-    for (i = 0; i < data.length; i++) {
-      (data[i][0] >= 55 && data[i][1] > 7) ? category.push('Senior') : category.push('Open')
-    }
-    return category
+    return data.map(item => item[0] >= 55 && item[1] > 7 ? 'Senior' : 'Open')
   }
