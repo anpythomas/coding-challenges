82e5832d2c8c49cd0564e0efc8b73750e4724d9e
Andy <andythomas.py@gmail.com>
2023-05-16 15:31:49 -0500

    Added 5-16 challenge 1 v.1.2

--- 5-16-2023/categorizeNewMember/js/main.js
+++ 5-16-2023/categorizeNewMember/js/main.js
@@ -1,11 +1,7 @@
 function openOrSenior(data){
     let category = []
     for (i = 0; i < data.length; i++) {
-      if (data[i][0] >= 55 && data[i][1] > 7) {
-        category.push('Senior')
-      } else {
-        category.push('Open')
-      }
+      (data[i][0] >= 55 && data[i][1] > 7) ? category.push('Senior') : category.push('Open')
     }
     return category
   }
