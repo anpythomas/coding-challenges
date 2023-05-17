691002b47e5f2ee023f13c640ac1c9d7608769ce
Andy <andythomas.py@gmail.com>
2023-05-17 09:41:10 -0500

    Added 5-17 challenge 1 version v.1.2

--- getTheMeanOfAnArray/js/main.js
+++ getTheMeanOfAnArray/js/main.js
@@ -1,11 +1,8 @@
 function getAverage(marks){
-    let sum = 0
     let avg = 0
     let length = marks.length
     
-    for (i = 0; i < length; i++) {
-      sum += marks[i]
-    }
+    let sum = marks.reduce((acc, cv) => acc + cv)
   
     avg = Math.round(sum / length)
       
