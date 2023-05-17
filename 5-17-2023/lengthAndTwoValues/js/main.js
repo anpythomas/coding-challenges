d9555265ee5df5f8c3c21f5e69e923f7f67a9f58
Andy <andythomas.py@gmail.com>
2023-05-17 09:59:45 -0500

    Added 5-17 challenge 2 v.1.3

--- 5-17-2023/lengthAndTwoValues/js/main.js
+++ 5-17-2023/lengthAndTwoValues/js/main.js
@@ -2,12 +2,7 @@ function alternate(n, firstValue, secondValue){
     let finalArray = []
     
     for (i = 0; i < n; i++) {
-      if (i % 2 === 0) {
-        finalArray.push(firstValue)
-      }
-      else {
-        finalArray.push(secondValue)
-      }
+      i % 2 === 0 ? finalArray.push(firstValue) : finalArray.push(secondValue)
     }
     
     return finalArray
