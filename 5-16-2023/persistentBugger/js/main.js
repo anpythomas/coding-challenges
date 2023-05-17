f00f26bcf451c7aa20432bbd2303cac5c0bde7f3
Andy <andythomas.py@gmail.com>
2023-05-16 21:44:23 -0500

    Added 5-16 challenge 2 v.1.2

--- 5-16-2023/persistentBugger/js/main.js
+++ 5-16-2023/persistentBugger/js/main.js
@@ -1,18 +1,13 @@
 function persistence(num) {
     let counter = 0
-    let numToStr = num.toString()
+    num = num.toString()
     
-     while (numToStr.length !== 1) {
-       let tmpProduct = 1
- 
-       for (i = 0; i < numToStr.length; i++) {
-         tmpProduct *= Number(numToStr[i])  
-       }
-       
-       counter += 1
-       numToStr = tmpProduct.toString()
-       
+    while (num.length !== 1) {
+        num = num.split('').reduce((total, cv) => total * cv, 1).toString()
+        counter += 1
      }
       
      return counter
  }
+
+ console.log(persistence(39))
