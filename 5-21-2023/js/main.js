05cedd743840f3df6d3e15da3b4a6bd6de9dc26d
Andy <andythomas.py@gmail.com>
2023-05-21 22:31:39 -0500

    Added 5-21 challenge 1 solution v.1.2

--- 5-21-2023/js/main.js
+++ 5-21-2023/js/main.js
@@ -1,5 +1,3 @@
 function nameShuffler(str){
-    let nameList = str.split(' ')
-    let nameReversed = `${nameList[1]} ${nameList[0]}`
-    return nameReversed
+    return `${str.split(' ')[1]} ${str.split(' ')[0]}`;
   }
