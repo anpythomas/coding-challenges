92f05698b781c8bd063e2528b30f8adc0b950c4e
Andy <andythomas.py@gmail.com>
2023-05-27 16:11:22 -0500

    Added 5-27 challenge 1 solution v.1.4

--- 5-27-2023/countOfPosSumOfNeg/js/main.js
+++ 5-27-2023/countOfPosSumOfNeg/js/main.js
@@ -1,7 +1,15 @@
 function countPositivesSumNegatives(input) {
     let newArray = [0, 0]
     
-    input.map((elem, i) => input[i] > 0 ? newArray[0] += 1 : newArray[1] += input[i])
+    try {
+      input.map((elem, i) => input[i] > 0 ? newArray[0] += 1 : newArray[1] += input[i])
+    } catch {
+      return []
+    }
+    
+    if (newArray[0] === 0 && newArray[1] === 0) {
+      return []
+    }
     
     return newArray 
   }
