d7d185759414cff87711e41118d7e316c74a9c37
Andy <andythomas.py@gmail.com>
2023-05-29 13:45:30 -0500

    Added 5-29 challenge 1 solution v.1.2

--- 5-28-2023/countingSheep/js/main.js
+++ 5-28-2023/countingSheep/js/main.js
@@ -1,9 +1,7 @@
 function countSheeps(arrayOfSheep) {
     let count = 0
     for (i = 0; i < arrayOfSheep.length; i++) {
-      if (arrayOfSheep[i] === true) {
-        count+=1
-      }
+      arrayOfSheep[i] === true ? count+=1 : 'pass'
     }
     return count
   }
