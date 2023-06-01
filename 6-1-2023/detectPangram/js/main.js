7c6487c311038c35b280e8e8773b1a1da8084dd3
Andy <andythomas.py@gmail.com>
2023-06-01 14:47:38 -0500

    Added 6-1 challenge 1 solution v.1.2

--- 6-1-2023/detectPangram/js/main.js
+++ 6-1-2023/detectPangram/js/main.js
@@ -5,22 +5,13 @@ function isPangram(string){
     // Iterate through sentence
     for (i = 0; i < string.length; i++) {
       
-      // Check if char (converted to lowercase) is a letter 
+      // Check if char (converted to lowercase) is a letter and not in empty list, then add to empty list
       const tmpLwrStr = string[i].toLowerCase()
-      if (tmpLwrStr.charCodeAt(0) >= '97' && tmpLwrStr.charCodeAt(0) <= '122') {
-            
-        // and not in empty list
-        if (!lettersUsed.includes(tmpLwrStr)) {
-          
-          // If both tests passed add to empty list
-          lettersUsed.push(tmpLwrStr)
-        }
+      if (tmpLwrStr.charCodeAt(0) >= '97' && tmpLwrStr.charCodeAt(0) <= '122' && !lettersUsed.includes(tmpLwrStr)) {
+        lettersUsed.push(tmpLwrStr)            
       }
     }  
-  
-    // Count number of letters in list
-    const totalCharsCount = lettersUsed.length
     
     // If count is 26, return true. Else, return false
-    return totalCharsCount === 26 ? true : false
+    return lettersUsed.length === 26 ? true : false
   }
