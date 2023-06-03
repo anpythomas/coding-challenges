9a93e29146f1af4643d72d9d66621cd8907ff7db
Andy <andythomas.py@gmail.com>
2023-06-03 18:22:16 -0500

    Added 6-3 challenge 2 v1.2

--- 6-3-2023/elimTheIntruders/js/main.js
+++ 6-3-2023/elimTheIntruders/js/main.js
@@ -1,13 +1,3 @@
 function eliminateUnsetBits(number) {
-    console.log(number, typeof number)
-    
-    // Iterarte through string 
-    number = number.replaceAll('0', '')
-    console.log(number)
-    
-    // convert string to binary
-    number = parseInt(number, 2)
-    
-    // return binary binary object
-    return number ? number : 0
+    return parseInt(number.replaceAll('0', ''), 2) ? parseInt(number.replaceAll('0', ''), 2) : 0
   }
