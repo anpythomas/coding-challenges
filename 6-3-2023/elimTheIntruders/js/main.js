e572d045be7b1f2394a2d7f126f7bc37d9af5efe
Andy <andythomas.py@gmail.com>
2023-06-03 18:26:37 -0500

    Added 6-3 challenge 2 v1.3

--- 6-3-2023/elimTheIntruders/js/main.js
+++ 6-3-2023/elimTheIntruders/js/main.js
@@ -1,3 +1,7 @@
 function eliminateUnsetBits(number) {
-    return parseInt(number.replaceAll('0', ''), 2) ? parseInt(number.replaceAll('0', ''), 2) : 0
-  }
+    // Remove all unset bits ('0') and convert binary string to int
+    let result = parseInt(number.replaceAll('0', ''), 2)
+    
+    // If result is truthy, return result. Else, return 0
+    return result ? result : 0
+}
