f5bb0ccb44f17d097b13c1c4515ca132ac6b65c6
Andy <andythomas.py@gmail.com>
2023-06-04 19:41:12 -0500

    Added 6-3 challenge 3 v.1.0

---
+++ 6-4-2023/sumOfTheFirstNthTermOfSeries/js/main.js
@@ -0,0 +1,18 @@
+function SeriesSum(n) {
+    let myInt = 0
+    let quotient = 4
+
+    for (i = 0; i < n; i++) {
+        if (i === 0) {
+            myInt += 1
+        } else if (i === 1) {
+            myInt += 1/quotient
+        } else {
+            myInt += 1/(quotient+3*(i-1))
+        }
+    }
+
+    return `${myInt.toFixed(2)}`
+}
+
+console.log(seriesSum(4))
