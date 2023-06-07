5434dd8e47bf549146a5b0d39cb3385d931dc361
Andy <andythomas.py@gmail.com>
2023-06-06 23:06:44 -0500

    Added 6-6 challenge 1 v.1.2

--- 6-6-2023/isItPalindrome/js/main.js
+++ 6-6-2023/isItPalindrome/js/main.js
@@ -2,14 +2,9 @@ function isPalindrome(x) {
     let xHalfLength = (x.length / 2)
     let xLower = x.toLowerCase()
     
-    if (x.length === 0 || x.length === 1) {
-      return true
-    }
     
     for (i = 0; i < xHalfLength; i++) {
-      if (xLower[i] === xLower[x.length-1-i]) {
-        'pass'
-      } else {
+      if (xLower[i] !== xLower[x.length-1-i]) {
         return false
       }
     }
