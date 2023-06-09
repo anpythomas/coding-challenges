a39bf1ed6093b34fd8e4f73b1126cb9d67f8df81
Andy <andythomas.py@gmail.com>
2023-06-09 07:48:06 -0500

    Added 6-9 2 v.1.2

--- 6-9-2023/partsOfAList/js/main.js
+++ 6-9-2023/partsOfAList/js/main.js
@@ -4,20 +4,17 @@ function partlist(arr) {
     
     // Iterate through original array
     for (i = 0; i < arr.length-1; i++) {
-
       // Create empty tmp array
       let tmpArray = []
       
       // For each iteration, conatenate elements from i back to zero into string and add to tmp array
-      let tmpString = arr.slice(0, i+1).join(' ')
-      tmpArray.push(tmpString)
+      tmpArray.push(arr.slice(0, i+1).join(' '))
       
       // Then concat all items from i+1 through last element into a single string and add to tmp array
-      let tmpString2 = arr.slice(i+1, arr.length).join(' ')
-      tmpArray.push(tmpString2)
-
-        // Add tmp array to newArray
-        newArray.push(tmpArray)
+      tmpArray.push(arr.slice(i+1, arr.length).join(' '))
+      
+      // Add tmp array to newArray
+      newArray.push(tmpArray)
     }
   
     // Return newArray
