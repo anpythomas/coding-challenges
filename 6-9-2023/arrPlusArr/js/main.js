956ab25fc0a0d2c0e4f0f0f37ebbcbd4e1cad072
Andy <andythomas.py@gmail.com>
2023-06-09 07:09:35 -0500

    Added 6-9 1 v.1.3

--- 6-9-2023/arrPlusArr/js/main.js
+++ 6-9-2023/arrPlusArr/js/main.js
@@ -1,15 +1,11 @@
-function arrayPlusArray(arr1, arr2) {
-    const initialValue = 0;
-    
+function arrayPlusArray(arr1, arr2) {  
     arr1 = arr1.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
-      initialValue
+      0
     );
     
-    arr2 = arr2.reduce(
+    return arr2 = arr2.reduce(
       (acc, cv) => acc + cv,
       arr1
     );
-    
-    return arr2; //something went wrong
   }
