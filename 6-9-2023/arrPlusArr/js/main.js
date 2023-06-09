ae99cce053208200381a0dbfb62e55e18ee99d74
Andy <andythomas.py@gmail.com>
2023-06-09 07:14:19 -0500

    Added 6-9 1 v.1.4

--- 6-9-2023/arrPlusArr/js/main.js
+++ 6-9-2023/arrPlusArr/js/main.js
@@ -1,11 +1,10 @@
 function arrayPlusArray(arr1, arr2) {  
-    arr1 = arr1.reduce(
+    const newArray = arr1.concat(arr2)
+    
+    return newArray.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       0
     );
     
-    return arr2 = arr2.reduce(
-      (acc, cv) => acc + cv,
-      arr1
-    );
+  
   }
