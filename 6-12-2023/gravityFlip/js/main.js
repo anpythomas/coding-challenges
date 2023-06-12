f2132c8d6b9d2999a686e9388c28961e849bb5d7
Andy <andythomas.py@gmail.com>
2023-06-12 07:48:52 -0500

    Added 6-12 2 v.1.2

--- 6-12-2023/gravityFlip/js/main.js
+++ 6-12-2023/gravityFlip/js/main.js
@@ -1,11 +1,3 @@
 const flip=(d, a)=>{
-  
-    if (d === 'R') {
-      a.sort()
-    } else if (d === 'L') {
-      a.sort(function(a, b){return b-a});
-  
-    }
-    
-    return a
+    return d === 'R' ? a.sort() : a.sort(function(a, b){return b-a});
   }
