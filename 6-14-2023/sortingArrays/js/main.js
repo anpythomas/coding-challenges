360d38c617e6e859524e6b1ce12c527b09520fc8
Andy <andythomas.py@gmail.com>
2023-06-14 15:05:43 -0500

    Added 6-14 1 v.1.2 js. Less code buy less efficient without break in conditional

--- 6-14-2023/sortingArrays/js/main.js
+++ 6-14-2023/sortingArrays/js/main.js
@@ -2,15 +2,11 @@ function sortArray(a1, a2) {
     let newArray = []
     
     for (i = 0; i < a1.length; i++) {
-      console.log(a1[i][0])
       
       for (n = 0; n < a2.length; n++) {
-        console.log(a2[n][0])
         
-        if (a1[i][0] === a2[n][0]) {
-          newArray.push(a2[n])
-          break
-        }
+        (a1[i][0] === a2[n][0]) ? newArray.push(a2[n]) : 'pass'
+        
       }
     }
     
