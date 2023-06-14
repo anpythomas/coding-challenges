4dcd0ff64892b81ee173886297a90898f46eaf15
Andy <andythomas.py@gmail.com>
2023-06-14 15:33:07 -0500

    Added 6-14 2 v.1.3 python

--- 6-14-2023/flattenAndSortArray/main.py
+++ 6-14-2023/flattenAndSortArray/main.py
@@ -6,23 +6,8 @@ def flatten_and_sort(array):
     for nestedArray in array:
         for item in nestedArray:
             consolodatedSortedNums.append(item)
-                
-    # Once all nums added to consolodatedSortedNums, Iterate through consolodatedSortedNums and sort in asscending order
-    # Create infinite while loop
-    arrSorted = False
-    while arrSorted == False:
-        # Set arrSorted to true
-        arrSorted = True
-        
-        # Iterate through consolodatedSortedNums with length - 1
-        for i in range(len(consolodatedSortedNums) - 1):
-            
-            # If element at index i is higher than element at index i+1, then reverse places and set arrSorted to false
-            if consolodatedSortedNums[i] > consolodatedSortedNums[i+1]:
-                tmpVal = consolodatedSortedNums[i]
-                consolodatedSortedNums[i] = consolodatedSortedNums[i+1]
-                consolodatedSortedNums[i+1] = tmpVal
-                arrSorted = False
     
-    # Return consolodatedSortedNums
+    # Sort array
+    consolodatedSortedNums.sort()
+    
     return consolodatedSortedNums
