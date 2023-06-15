1ca6b83ff0ae360519f255415eb513397c1bd10e
Andy <andythomas.py@gmail.com>
2023-06-15 10:32:42 -0500

    Added 6-15 2 v.1.2 js

--- 6-15-2023/insertDashes/js/main.js
+++ 6-15-2023/insertDashes/js/main.js
@@ -4,24 +4,19 @@ function insertDash(num) {
     
     // Convert num to string
     num = String(num)
-      
-    // Create variable called minNumOfOdds and set to zero
-    minNumOdds = 0
+  
     
     // Iterate thorugh each item of string
     for (i = 0; i < num.length-1; i++) {
       // Add each char to myNewString
       myNewString += num[i]
       
-      // For each char, convert to number and check if number is odd. If yes, check if minNumOdds equals 1
-      if ((Number(num[i]) % 2 != 0) && (Number(num[i+1]) % 2 != 0) && (num.length-1 != i)) {
-        myNewString += "-"
-      }
-    }
-    
-    // Add last char of num to myNewString
-    myNewString += num[num.length-1]
+      // For each char, convert to number and check if number is odd and if next number is odd
+      Number(num[i]) % 2 != 0 && Number(num[i+1]) % 2 != 0 && num.length-1 != i ? myNewString += "-" : 'pass'
+      
+    } 
   
-    return myNewString
+    // Return myNewString + last char from num
+    return myNewString += num[num.length-1]
   }
   
