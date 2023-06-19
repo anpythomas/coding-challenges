3f063f28233f3b233e8190c7b0d02fa4d03704d5
Andy <andythomas.py@gmail.com>
2023-06-18 19:53:31 -0500

    Added 6-17 1 v.1.2 python

--- 6-17-2023/digitDigit/main.py
+++ 6-17-2023/digitDigit/main.py
@@ -1,16 +1,10 @@
 def square_digits(num):
     emptyString = ""
     
-    # Convert number to string
-    numToStr = str(num)
-    
-    # Iterate through string
-    for char in numToStr:
-        # For each item, convert to int and squar
-        tmpIntSquared = int(char) * int(char)
-        
-        # convert back to string and add to emptyString
-        emptyString += str(tmpIntSquared)        
+    # Convert number to string iterate through string
+    for char in str(num):
+        # For each item, convert to int, square, convert back to string and add to empty string        
+        emptyString += str(int(char) ** 2)        
         
     # Return emptyString converted to int
     return int(emptyString)
