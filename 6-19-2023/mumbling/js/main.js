9564112b918f5ff2527c28fee3b92f35c8e7c698
Andy <andythomas.py@gmail.com>
2023-06-20 20:23:52 -0500

    Added 6-19 4 v.1.2 js-strings

--- 6-19-2023/mumbling/js/main.js
+++ 6-19-2023/mumbling/js/main.js
@@ -8,18 +8,15 @@ function accum(s) {
     
       // For the rest of the numbers upto i+1 add lowercase of same letter
     for (n = 0; n < i+1; n++) {
+      
       // First iteration, add letter capitalized one time to string
-      if (n === 0) {
-        newString += s[i].toUpperCase()
-      } else {
-        newString += s[i].toLowerCase()
-      }
+      (n === 0) ? newString += s[i].toUpperCase() : newString += s[i].toLowerCase()
+
     }
     
     // Once loop is complete add a dash to newString if not last char in s
-    if (i != s.length-1) {
-      newString += '-'
-    }
+    (i != s.length-1) ? newString += '-' : 'pass'
+    
   }
   
   // Return newString
