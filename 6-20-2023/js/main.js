34c250138f8de95ffd5afa80d37776b407744f96
Andy <andythomas.py@gmail.com>
2023-06-21 11:26:14 -0500

    Added 6-20 1 v.1.2 js-strings

--- 6-20-2023/js/main.js
+++ 6-20-2023/js/main.js
@@ -1,7 +1,4 @@
 function solution(a, b){
-    if (a.length > b.length) {
-      return `${b}${a}${b}`
-    } else {
-      return `${a}${b}${a}`
-    } 
+    return a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
 }
+  
