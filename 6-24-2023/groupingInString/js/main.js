adaab980000f4cf854a7073444975251b3bcb059
Andy <andythomas.py@gmail.com>
2023-06-24 12:43:40 -0500

    Added 6-24 2 v.1.2 js-strings

--- 6-24-2023/groupingInString/js/main.js
+++ 6-24-2023/groupingInString/js/main.js
@@ -1,18 +1,12 @@
 const isConsecutive = (str) => {
-  
-    // Create blank array to hold numbers already seen called pastNums
-    let pastNums = []
     
     // Iterate through str, starting at second position (index 1)
     for (i = 1; i < str.length; i++) {
       
       // If current element in pastNums array and current element not equal to previous element, return false
-      if (pastNums.includes(str[i]) && str[i] != str[i-1]) {
+
+      if (str.slice(0, i).includes(str[i]) && str[i] != str[i-1]) {
         return false
-        
-        // Else, add element to pastNums array
-      } else {
-        pastNums.push(str[i])
       }
       
     }
@@ -20,3 +14,5 @@ const isConsecutive = (str) => {
     // Return true if false not returned after loop completed
     return true
   }
+
+console.log(isConsecutive('001122'))
