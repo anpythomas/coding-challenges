66dd9e720d838a0290a190e2a0e825de2c5e5f3b
Andy <andythomas.py@gmail.com>
2023-06-24 12:07:33 -0500

    Added 6-24 1 v.1.0 js-arrays

---
+++ 6-24-2023/js/main.js
@@ -0,0 +1,15 @@
+function monkeyCount(n) {
+    // Create empty array called monkeyCount
+    let monkeyCount = []
+    
+    // Create for loop from 1 to n
+    for (i = 1; i <= n; i++) {
+      
+      // For each iteration, add iterator to the end of the array monkeyCount
+      monkeyCount.push(i)
+      
+    }
+    
+    // return monkeyCount
+    return monkeyCount
+  }
