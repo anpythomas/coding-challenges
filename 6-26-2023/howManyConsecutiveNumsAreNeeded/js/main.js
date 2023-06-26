689c7d88f3e4f7fe78ae2020547b5c8632711898
Andy <andythomas.py@gmail.com>
2023-06-26 15:58:33 -0500

    Added 6-26 1 v.1.0 js-arrays

---
+++ 6-26-2023/howManyConsecutiveNumsAreNeeded/js/main.js
@@ -0,0 +1,23 @@
+function compareNumbers(a, b) {
+    return a - b;
+  }
+  
+  function consecutive(arr) {
+    // Sort array
+    arr.sort(compareNumbers)
+    
+    // Create counter variable
+    let ctr = 0
+    
+    // Create loop starting at lowest number and iterating through highest number
+    for (i = arr[0]; i <= arr[arr.length-1]; i++) {
+      
+      // If a number is missing, increate ctr variable by 1
+      if (!arr.includes(i)) {
+        ctr += 1
+      }
+    }
+    
+    // Return counter variable
+    return ctr
+  }
