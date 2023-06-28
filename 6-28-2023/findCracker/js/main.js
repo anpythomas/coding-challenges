52bba0d0e81e4126f13325da956de1681901a487
Andy <andythomas.py@gmail.com>
2023-06-28 09:12:04 -0500

    Added 6-28 2 v.1.0 js-arrays

---
+++ 6-28-2023/findCracker/js/main.js
@@ -0,0 +1,40 @@
+function findHack(arr) {
+    let gradeValues = {'A': 30, 'B': 20, 'C': 10, 'D': 5}
+    
+    // Declare empty array called hackedNamesArray
+    let hackedNamesArray = []
+    
+    // Iterate through arr
+    for (i = 0; i < arr.length; i++) {
+  
+      for (n = 2; n < arr[i].length; n++) {
+        
+        let tmpSum = 0
+        let badStudent = false
+        for (z = 0; z < arr[i][n].length; z++) {
+          
+          // If letter grade in gradeValues, add value. If not do nothing
+          if (gradeValues[arr[i][n][z]]) {
+            tmpSum += gradeValues[arr[i][n][z]]
+          }
+          
+          // If grade not equal to 'A' or 'B' set badStudent to true
+          if (arr[i][n][z] !== 'A' && arr[i][n][z] !== 'B') {
+            badStudent = true
+          }
+        }
+        
+        // If 5 or more courses and badStudent = false, extra 20 points added to tmpSum
+        if (arr[i][2].length >= 5 && badStudent === false) {
+          tmpSum += 20
+        }
+  
+        // If tmpSum greater than 200 or tmpSum not equal to points in index 1, add 'name' to hackedNamesArray
+        if (tmpSum !== arr[i][1] || tmpSum > 200) {
+          hackedNamesArray.push(arr[i][0])
+        }      
+      }    
+    }
+    
+    return hackedNamesArray
+  }
