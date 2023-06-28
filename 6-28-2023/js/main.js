bd0563d7f65d3581e310d1a091657fbc6b556646
Andy <andythomas.py@gmail.com>
2023-06-28 08:14:00 -0500

    Added 6-28 1 v.1.2 js-arrays

--- 6-28-2023/js/main.js
+++ 6-28-2023/js/main.js
@@ -3,12 +3,10 @@ function allNonConsecutive (arr) {
     // Create empty array to add objects
     let myArray = []
    
-    
     for (i = 1; i < arr.length; i++) {
       
-      if (arr[i] - arr[i-1] != 1) {
-        myArray.push({i: i, n: arr[i]})
-      }
+      arr[i] - arr[i-1] != 1 ? myArray.push({i: i, n: arr[i]}) : 'pass'
+
     }
     
     return myArray
