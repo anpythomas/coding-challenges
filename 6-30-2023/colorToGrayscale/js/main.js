ef6e0609028ea77fda4297a2c84f2a10ea06b330
Andy <andythomas.py@gmail.com>
2023-06-30 15:40:10 -0500

    Added 6-30 1 v.1.2 js-strings

--- 6-30-2023/colorToGrayscale/js/main.js
+++ 6-30-2023/colorToGrayscale/js/main.js
@@ -1,21 +1,12 @@
 function rgbToGrayscale(color){ 
-    let color1 = color.slice(1, 3)
-    let color2= color.slice(3, 5)
-    let color3 = color.slice(5, 7)
     
-    let num1Raw = parseInt(color1, 16) * .299
-    let num2Raw = parseInt(color2, 16) * .587
-    let num3Raw = parseInt(color3, 16) * .114
+    let finalNum = Math.round((
+      parseInt(color.slice(1, 3), 16) * .299 + 
+      parseInt(color.slice(3, 5), 16) * .587 + 
+      parseInt(color.slice(5, 7), 16) * .114)
+    ).toString(16);
     
-    console.log(num1Raw, num2Raw, num3Raw)
     
-    let finalNum = Math.round((num1Raw + num2Raw + num3Raw)).toString(16)
+    return (finalNum.length === 1) ? '#' + (finalNum = '0' + finalNum).repeat(3) : '#' + finalNum.repeat(3);
     
-    console.log(finalNum, typeof finalNum)
-    
-    if (finalNum.length === 1) {
-      finalNum = '0' + finalNum
-    }
-    
-    return finalNum === '00' ? '#000000' : '#' + finalNum + finalNum + finalNum
   }
