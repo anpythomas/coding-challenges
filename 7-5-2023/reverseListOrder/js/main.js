dbe36e2ad4420074d467e6c8d4d509c9fd153361
Andy <andythomas.py@gmail.com>
2023-07-05 17:50:12 -0500

    Added 7-5 1 v.1.2 js-arrays

--- 7-5-2023/reverseListOrder/js/main.js
+++ 7-5-2023/reverseListOrder/js/main.js
@@ -1,10 +1,8 @@
 function reverseList(list) {
+  
     let newArray = []
     
-    for (i = list.length-1; i >= 0; i--) {
-      console.log(list[i])
-      newArray.push(list[i])
-    }
+    list.forEach(elem => newArray.unshift(elem))
     
     return newArray
   }
