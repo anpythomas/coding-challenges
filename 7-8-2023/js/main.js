79110ddb29ace59cf17ff7b3c8c214a47cf046e0
Andy <andythomas.py@gmail.com>
2023-07-08 14:26:46 -0500

    Add 7-8 1 v.1.0 js-strings

---
+++ 7-8-2023/js/main.js
@@ -0,0 +1,19 @@
+function stepThroughWith(s) {
+    // Declare variable called previousChar and set to empty string
+    let previousChar = ""
+    
+    // Iterate through string
+    for (i = 0; i < s.length; i++) {
+      // If current variable equals previousChar, return true
+      if (previousChar === s[i]) {
+        return true
+           
+      // Else, set previousChar to current char
+      } else {
+        previousChar = s[i]
+      }
+    }
+  
+    // If you make it to the end return false
+    return false
+  }
