a47408412ec7f6056064437dc35b126a56ea84fe
Andy <andythomas.py@gmail.com>
2023-08-23 13:51:03 -0500

    Add 8-23-2023 1 v.1.2 ts-strings 7kyu with map

--- 8-23-2023/jadenCasingStrings/dist/main.js
+++ 8-23-2023/jadenCasingStrings/dist/main.js
@@ -1,11 +1,7 @@
 "use strict";
 String.prototype.toJadenCase = function () {
-    let jCaseArray = this.split(" ");
-    for (let i = 0; i < jCaseArray.length; i++) {
-        jCaseArray[i] =
-            jCaseArray[i][0].toUpperCase() +
-                jCaseArray[i].slice(1, jCaseArray[i].length);
-    }
-    return jCaseArray.join(" ");
+    return this.split(" ")
+        .map((elem) => (elem = elem[0].toUpperCase() + elem.slice(1)))
+        .join(" ");
 };
-console.log(('How can mirrors be real if our eyes aren\'t real').toJadenCase());
+console.log("How can mirrors be real if our eyes aren't real".toJadenCase());
--- 8-23-2023/jadenCasingStrings/src/main.ts
+++ 8-23-2023/jadenCasingStrings/src/main.ts
@@ -1,13 +1,7 @@
 String.prototype.toJadenCase = function () {
-    let jCaseArray: string[] = this.split(" ");
-
-    for (let i = 0; i < jCaseArray.length; i++) {
-        jCaseArray[i] =
-            jCaseArray[i][0].toUpperCase() +
-            jCaseArray[i].slice(1, jCaseArray[i].length);
-    }
-
-    return jCaseArray.join(" ");
+    return this.split(" ")
+        .map((elem) => (elem = elem[0].toUpperCase() + elem.slice(1)))
+        .join(" ");
 };
 
 interface String {
@@ -15,4 +9,4 @@ interface String {
     toJadenCase(): string;
 }
 
-console.log(('How can mirrors be real if our eyes aren\'t real').toJadenCase())
+console.log("How can mirrors be real if our eyes aren't real".toJadenCase());
