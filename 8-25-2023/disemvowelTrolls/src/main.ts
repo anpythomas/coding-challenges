8d28a150b995c973ba1e01f3e817cc9e194e415e
Andy <andythomas.py@gmail.com>
2023-08-25 13:17:33 -0500

    Add 8-25 1 v.1.2 ts-strings 7kyu with filter and join

--- 8-25-2023/disemvowelTrolls/src/main.ts
+++ 8-25-2023/disemvowelTrolls/src/main.ts
@@ -1,21 +1,7 @@
-function disemvowel(str: string): string {
-    // Create string with vowels called allVowels
-    let allVowels: string = "aeiou";
-
-    // Create final string to return called cleanString
-    let cleanString: string = "";
-
-    // Iterate through string
-    for (let i = 0; i < str.length; i++) {
-        // If char NOT in allVowels string, add element to cleanString
-        if (!allVowels.includes(str[i].toLowerCase())) {
-            cleanString += str[i];
-        }
-    }
-
-    // return a string called cleanString
-    return cleanString;
+export const removeVowels = (originalComment: string) :string => {
+    const vowels = 'aeiouAEIOU'
+    
+    return [...originalComment].filter(char => !vowels.includes(char)).join(' ')
 }
 
-
-console.log(disemvowel('How arE you?'))
+console.log(removeVowels('Hello, worlD'))
